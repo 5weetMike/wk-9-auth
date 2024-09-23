18f3fc157328ec060895392820de76d4a77425fb
Michael Hampson <mdavidhampson@gmail.com>
2024-09-23 12:27:44 +0100

    chore: middleware example

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const  exampleRouter = require("./middleware");
 
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+app.use (exampleRouter);
 
 
 
